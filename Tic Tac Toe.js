40986d69494990fe88c1017c8c9273e561e87e21
Spencer Serrao <ikaikaserrao@gmail.com>
2020-09-04 13:58:05 -1000

    changes

--- Tic Tac Toe.js
+++ Tic Tac Toe.js
@@ -4,7 +4,7 @@ var gameBoard =  [0,1,2,
 				  3,4,5,
 				  6,7,8];
 
-var winningCombinations = 
+var combo = 
 				[[0,1,2],	// 0
 				 [3,4,5],	// 1
 				 [6,7,8],	// 2
@@ -49,4 +49,31 @@ else{
 		}
 	}
 	 
-	
+	for(var i=0; i<combo.length; i++){
+		if(combo[i] == ["x","x","x"])
+	}
+
+
+
+
+
+
+// I don't understand why the console.log(combo[i]) is logging [3,4,5] twice
+var combo = 
+				[["x","x","x"],	// 0
+				 [3,4,5],	// 1
+				 [6,7,8],	// 2
+				 [0,3,6],	// 3
+				 [1,4,7],	// 4
+				 [2,5,8],	// 5
+				 [0,4,8],	// 6
+				 [2,4,6]];	// 7
+
+	for(var i=0; i<combo.length; i++){
+		if(combo[i] == 3,4,5){
+		console.log(combo[i]);
+		}	
+		else{
+			console.log("sorry")
+		}
+	}
--- practice.js
+++ practice.js
@@ -1,38 +1,41 @@
 alert("Welcome to Tic-Tac-Toe!!!")
 
-var gameBoard =  ["0","1","2","3","4","5","6","7","8"];
+var gameBoard =  [0,1,2,
+				  3,4,5,
+				  6,7,8];
 
-playerX();
+var combo = 
+				[[0,1,2],	// 0
+				 [3,4,5],	// 1
+				 ["x", "x", "x"],	// 2
+				 [0,3,6],	// 3
+				 [1,4,7],	// 4
+				 ["x", "x", "x"],	// 5
+				 [0,4,8],	// 6
+				 [2,4,6]];	// 7
 
-	function playerX(){
-		var choice = prompt("Input the numbered spot you want to place X in: ");
-
-			if((gameBoard[choice]) == choice){
-			gameBoard[choice] == "x";
-			}
-			else{
-			prompt("This spot is taken. Choose another numbered spot: ");
-			}
+	for(var i=0; i<combo.length; i++){
+		if(combo[i] != ["x","x","x"]){
+		console.log(combo[i]);
+		}
 	}
-	
-console.log(gameBoard);
 
 
-alert("Welcome to Tic-Tac-Toe!!!")
+var combo = 
+				[["x","x","x"],	// 0
+				 [3,4,5],	// 1
+				 [6,7,8],	// 2
+				 [0,3,6],	// 3
+				 [1,4,7],	// 4
+				 [2,5,8],	// 5
+				 [0,4,8],	// 6
+				 [2,4,6]];	// 7
 
-var gameBoard =  ["0","1","2","3","4","5","6","7","8"];
-
-playerX();
-
-	function playerX(){
-		var choice = prompt("Input the numbered spot you want to place X in: ");
-
-			if((gameBoard[choice]) == choice){
-				gameBoard[choice] = "x"
-			}
-			else{
-			prompt("This spot is taken. Choose another numbered spot: ");
-			}
+	for(var i=0; i<combo.length; i++){
+		if(combo[i] != 3,4,5){
+			console.log(combo[i]);
+		}
+		else{
+			console.log("sorry")
+		}
 	}
-	
-console.log(gameBoard);
