2dc44b7c5173eab7547ae09450534d8a4efd20b4
John Supsupin <jsupsupin@village88.com>
2020-09-15 17:51:24 +0800

    Update Final Tic Tac Toe.js

--- Final Tic Tac Toe.js
+++ Final Tic Tac Toe.js
@@ -4,8 +4,7 @@ var gameBoard =  [0,1,2,
 				  3,4,5,
 				  6,7,8];
 
-var combo = 								
-				[[0,1,2],	// 0			
+var combo = 	[[0,1,2],	// 0			
 				 [3,4,5],	// 1
 				 [6,7,8],	// 2
 				 [0,3,6],	// 3
@@ -14,129 +13,51 @@ var combo =
 				 [0,4,8],	// 6
 				 [2,4,6]];	// 7
 
+var player_x = "x";
+var player_o = "o";
+
 randomStart();
 
-	function randomStart(){
-		if(Math.round(Math.random())>0){
-			X = "first";
-			O = "second";
-		}
-		else{
-			O = "first";
-			X = "second";
-		}
+function randomStart(){
+	var first_player = player_x; 
+	if(Math.round(Math.random())>0){
+		first_player = player_o;
 	}
 
-
-if(x = "first"){
-	alert("Player X is first.");
-	playerX();
-}
-else{
-	alert("Player O is first.");
-	playerO();
+	alert(`Player ${first_player} is first.`);
+	play(first_player);
 }
 
 //player X turn to choose a spot
-	function playerX(){
+function play(current_player){
+
+	var tile_count = 9; 
+	var choice = prompt(`Input the numbered spot you want to place ${current_player} in: `);
 	
-		var choice = prompt("Input the numbered spot you want to place X in: ");
-		for(var i=0; i<gameBoard.length; i++)													//how do i make this loop unending: until it hits the "break"
-			if((gameBoard[choice]) == "x" || (gameBoard[choice]) == "o" ){
-				var choice = prompt("Sorry Pick a new spot Input the numbered spot you want to place X in: ");
-		
+	if(tile_count > 0){
+		for(var i=0; i<gameBoard.length; i++){
+			if( (gameBoard[choice]) == player_x || (gameBoard[choice]) == player_o ){
+				choice = prompt(`Sorry Pick a new spot Input the numbered spot you want to place ${current_player} in: `);
 			}
 			else{
-				gameBoard[choice] = "x";
+				gameBoard[choice] = current_player;
+				tile_count--; 
 				break;
 			}
-	
-	 
-//player X did this connect 3 in a row?
-
-		for(var i = 0; i<combo.length; i++){
-			let xxx = [];
-			xxx.push(gameBoard[combo[i][0]]);
-			xxx.push(gameBoard[combo[i][1]]);
-			xxx.push(gameBoard[combo[i][2]]);
-		
-			if(JSON.stringify(xxx) == JSON.stringify(["x","x","x"])){
-				alert("You Win");
-				return;
-			}
-			else{ 
-				xxx = [];
-			}
-		}
-
-//player X are all indexes filled in the array?
-		var sum = 0;
-
-		for(var i=0;i<gameBoard.length; i++){
-				if(gameBoard[i]<9){
-					sum += gameBoard[i];
-				}
-		}
-		if(sum = 0){
-				alert("End Game. It's a tie.");
-		}
-		else{
-			playerO();
 		}
 	}
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//player X turn to choose a spot
-	function playerO(){
+	else{
+		alert("End Game. It's a tie.");
+	}
 	
-		var choice = prompt("Input the numbered spot you want to place O in: ");
-		for(var i=0; i<gameBoard.length; i++)													//how do i make this loop unending: until it hits the "break"
-			if((gameBoard[choice]) == "x" || (gameBoard[choice]) == "o" ){
-				var choice = prompt("Sorry Pick a new spot Input the numbered spot you want to place O in: ");
-		
-			}
-			else{
-				gameBoard[choice] = "o";
-				break;
-			}
+	var winning_line = { x: "x,x,x", o: "o,o,o" };
 	
-	 
-//player X did this connect 3 in a row?
-
-		for(var i = 0; i<combo.length; i++){
-			let ooo = [];
-			ooo.push(gameBoard[combo[i][0]]);
-			ooo.push(gameBoard[combo[i][1]]);
-			ooo.push(gameBoard[combo[i][2]]);
-		
-			if(JSON.stringify(ooo) == JSON.stringify(["o","o","o"])){
-				alert("You Win");
-				return;
-			}
-			else{
-				ooo = [];
-			}
-		}
-
-//player X are all indexes filled in the array?
-		var sum = 0;
-
-		for(var i=0;i<gameBoard.length; i++){
-				if(gameBoard[i]<9){
-					sum += gameBoard[i];
-				}
-		}
-		if(sum = 0){
-				alert("End Game. It's a tie.")
-		}
-		else{
-			playerX();
+	for(var i = 0; i<combo.length; i++){
+		if(gameBoard[combo[i]].join() == winning_line[current_player]){
+			alert("You Win");
+			break;
 		}
 	}
+	
+	(current_player === "x") ? play(player_o) : play(player_x);
+}
